152a68bca61f11477b0ff71d50775088aa5aea32
TutorIA-TFG <tutoria2025.tfg@gmail.com>
2025-07-14 17:15:14 +0200

    Reentrega - limpieza anterior

--- prueba.py
+++
@@ -1,6 +0,0 @@
-def filtrar_pares_descendente(lista):
-    # Filtra solo los números pares
-    pares = [num for num in lista if num % 2 == 0]
-    # Ordena la lista de pares en orden descendente
-    pares.sort(reverse=True)
-    return pares
